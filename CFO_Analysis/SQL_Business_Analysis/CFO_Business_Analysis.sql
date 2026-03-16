e2c37ebb0466742d7ec9a89c44955f09a89fdc8a
Uttam <uttam9695.com@gmail.com>
2026-03-16 21:42:49 +0530

    sql query update

--- CFO_Analysis/SQL_Business_Analysis/CFO_Business_Analysis.sql
+++ CFO_Analysis/SQL_Business_Analysis/CFO_Business_Analysis.sql
@@ -3,13 +3,13 @@ use Finance;
 SELECT 
     *
 FROM
-    ms_financial_data;
+    ms_financial_d;
 
 -- 1. Total Profit by each country.
 SELECT 
     Country, ROUND(SUM(profit), 2) AS Total_Profit
 FROM
-    ms_financial_data
+    ms_financial_d
 GROUP BY Country
 ORDER BY Total_Profit DESC;
 
@@ -17,7 +17,7 @@ ORDER BY Total_Profit DESC;
 SELECT 
     Product, ROUND(SUM(Sales), 2) AS Total_Sales
 FROM
-    ms_financial_data
+    ms_financial_d
 GROUP BY Product
 ORDER BY Total_Sales DESC
 LIMIT 3;
@@ -26,9 +26,9 @@ LIMIT 3;
 SELECT 
     COUNT(*) AS Orders
 FROM
-    ms_financial_data
+    ms_financial_d
 WHERE
-    `Discount Band` = 'No Discount'
+    Discount_Band = 'No Discount'
         AND Profit > 20000;
  
  -- 4
@@ -36,17 +36,17 @@ SELECT
     Product,
     ROUND((SUM(Profit) / SUM(Sales)) * 100, 2) AS Profit_Margin
 FROM
-    ms_financial_data
+    ms_financial_d
 GROUP BY Product;
  
  -- 5.
 SELECT 
     Year AS Yr,
-    `Month Name` AS Month_Name,
-    SUM(`Units Sold`) AS Total_Unit_Sold
+    Month_Name AS Month_Name,
+    SUM(Units_Sold) AS Total_Unit_Sold
 FROM
-    ms_financial_data
-GROUP BY Yr , `Month Name`
+    ms_financial_d
+GROUP BY Yr , Month_Name
 ORDER BY Total_Unit_Sold DESC;
  
  -- 6.
@@ -61,20 +61,41 @@ ORDER BY Total_Profit DESC;
  
  -- 7.
 SELECT 
-    `Discount Band`,
-    ROUND(AVG(`Units Sold`), 2) AS Units_Sold,
+    Discount_Band,
+    ROUND(AVG(Units_Sold), 2) AS Units_Sold,
     ROUND(SUM(Sales), 2) AS Total_Sales
 FROM
-    ms_financial_data
-GROUP BY `Discount Band`
+    ms_financial_d
+GROUP BY Discount_Band
 ORDER BY Units_Sold DESC;
  
 -- 8.
- 
+ select count(*) 
+ from ms_financial_d
+ where Sale_Price > (select avg(Sale_Price) Avg_SALES_PRICE
+ from ms_financial_d);
 
+ 
+ -- 9.
+ select Country, Product, sum(Profit) as Profit
+ from ms_financial_d
+ group by Country, Product
+ order by Profit
+ limit 3;
+ 
+ -- 10.
+select Product, round((sum(Gross_Sales) - sum(Sales)),2) as Total_Discount
+from ms_financial_d
+group by Product
+order by Total_Discount desc; 
+ 
+ -- 11.
+
+ 
+ 
  
  
 SELECT 
     *
 FROM
-    ms_financial_data;
+    ms_financial_d;
